fbe455185c4d8af211f52a8813a787fa4bbf542a
Andrew Boltachev <andrewboltachev@gmail.com>
2014-11-07 17:24:28 +0300

    htmler stuff

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from htmler.bootstrap import bootstrap3, container
+from htmler.fun import ul, li, div, h2, h4, a
+from htmler.tags import SafeString
+from django.http import HttpResponse
+from .models import Project, TimeEntry
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+
+def home(request):
+    data = [
+        (2014, [45, 46], [10, 11]),
+    ]
+    h_year = lambda year, weeks, months: div(h2(str(year)), weeks, months)
+    h_weeks_for_year = lambda *x: div(h4('Weeks'), ul(*x))
+    h_months_for_year = lambda *x: div(h4('Months'), ul(*x))
+    h_week = lambda m, y: li(a(str(m), href=reverse('weekly', kwargs={'year':y, 'week_no':m})))
+    h_month = lambda w, y : li(a(str(w), href=reverse('monthly', kwargs={'year':y, 'month_no':w})))
+    contents = [
+        h_year(y,
+            h_weeks_for_year(*[h_week(w, y) for w in b]),
+            h_months_for_year(*[h_month(m, y) for m in c])
+        )
+        for y, b, c in data
+    ]
+    html = bootstrap3(container(*contents))
+    return HttpResponse(html)
+
+
+def weekly(request, year, week_no):
+    return HttpResponse(html)
+
+
+def monthly(request, year, week_no):
+    pass
--- weakly_reports/urls.py
+++ weakly_reports/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'reports.views.home', name='home'),
+    url(r'^week/(?P<year>\d{4})/(?P<week_no>\d+)$', 'reports.views.weekly', name='weekly'),
+    url(r'^month/(?P<year>\d{4})/(?P<month_no>\d+)$', 'reports.views.monthly', name='monthly'),
 )
