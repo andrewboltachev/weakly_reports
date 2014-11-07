f7d56c71a0736c4617b63eb22adae994efeb96df
Andrew Boltachev <andrewboltachev@gmail.com>
2014-11-07 19:09:47 +0300

    for_week and for_month

--- reports/models.py
+++ reports/models.py
@@ -9,7 +9,19 @@ class Project(models.Model):
         return self.name
 
 
+class TimeEntryManager(models.Manager):
+    def for_month(self, year, month_no):
+        return TimeEntry.objects.filter(date__month=month_no, date__year=year)
+
+    def for_week(self, year, week_no):
+        from isoweek import Week
+        week = Week(year, week_no)
+        return TimeEntry.objects.filter(date__gte=week.monday(), date__lte=week.sunday())
+
+
 class TimeEntry(models.Model):
+    objects = TimeEntryManager()
+
     project = models.ForeignKey(Project)
     name = models.CharField(max_length=1024, blank=True)
     time = models.DecimalField(max_digits=3, decimal_places=1)
